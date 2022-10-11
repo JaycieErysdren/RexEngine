3d60be129c6d08b2936bf77e926c3f9a26c89f15
Jaycie Ewald <jaycie.ewald@outlook.com>
2022-10-11 17:03:58 -0500

    Liberator shrug

--- source/liberator/main.c
+++ source/liberator/main.c
@@ -18,14 +18,12 @@
 #include "rex.h"
 
 rex_window_external *window1;
-rex_window_external *window2;
 
 void main(int argc, char *argv[])
 {
 	Rex_Startup();
 
-	window1 = Rex_WindowExternal_Add("Liberator1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
-	window2 = Rex_WindowExternal_Add("Liberator2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+	window1 = Rex_WindowExternal_Add("Liberator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
 
 	while (REX_TRUE)
 	{
@@ -33,9 +31,6 @@ void main(int argc, char *argv[])
 
 		if (KEY_DOWN(SDL_SCANCODE_Q))
 			Rex_WindowExternal_Remove(window1);
-
-		if (KEY_DOWN(SDL_SCANCODE_C))
-			Rex_WindowExternal_Remove(window2);
 	}
 
 	Rex_Shutdown();
