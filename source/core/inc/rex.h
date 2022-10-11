6626941fc1b521d0a754e219dfa7bc3fe14e0da9
Jaycie Ewald <jaycie.ewald@outlook.com>
2022-10-10 21:07:18 -0500

    getting setup

---
+++ source/core/inc/rex.h
@@ -0,0 +1,16 @@
+// ========================================================
+//
+// FILE:			/source/core/inc/rex.h
+//
+// AUTHORS:			Jaycie Ewald
+//
+// PROJECT:			Rex Engine
+//
+// LICENSE:			ACSL 1.4
+//
+// DESCRIPTION:		Main Rex Engine header.
+//
+// LAST EDITED:		October 10th, 2022
+//
+// ========================================================
+
---
+++ source/liberator/main.c
@@ -0,0 +1,19 @@
+// ========================================================
+//
+// FILE:			/source/liberator/main.c
+//
+// AUTHORS:			Jaycie Ewald
+//
+// PROJECT:			Rex Engine
+//
+// LICENSE:			ACSL 1.4
+//
+// DESCRIPTION:		Liberator program entry point.
+//
+// LAST EDITED:		October 10th, 2022
+//
+// ========================================================
+
+// Include engine header
+#include "rex.h"
+
