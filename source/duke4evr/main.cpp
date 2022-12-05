79c0a251a732fd1e7c0cf9ea68fba8ac727c88cb
Jaycie Ewald <jaycie.ewald@outlook.com>
2022-12-05 02:25:24 -0600

    wall renderer

--- source/duke4evr/main.cpp
+++ source/duke4evr/main.cpp
@@ -23,6 +23,12 @@
 // Types
 //
 
+// 2-point integer vector
+typedef struct
+{
+	int32_t x, y;
+} vec2i_t;
+
 // 3-point integer vector
 typedef struct
 {
@@ -103,13 +109,13 @@ void ClipWall(scalar_t *x1, scalar_t *y1, scalar_t *z1, scalar_t x2, scalar_t y2
 	scalar_t da = *y1;
 	scalar_t db = y2;
 	scalar_t d = da - db;
-	if (d == 0) d = SCALAR(1);
-	scalar_t s = DIV(da, da - db);
+	if (d == SCALAR(0)) d = SCALAR(1);
+	scalar_t s = DIV(da, d);
 	*x1 = *x1 + MUL(s, x2 - (*x1));
 	*y1 = *y1 + MUL(s, y2 - (*y1));
 	*z1 = *z1 + MUL(s, z2 - (*z1));
 
-	if (*y1 == 0) *y1 = SCALAR(1);
+	if (*y1 == SCALAR(0)) *y1 = SCALAR(1);
 }
 
 //
@@ -265,103 +271,102 @@ int main(int argc, char *argv[])
 
 		// Render a world
 		{
-			scalar_t wv[4][3]; // World space vertices (x, y, z)
-			scalar_t pv1[4][3]; // Player space vertices (part 1) (x, y, z)
-			scalar_t pv2[4][3]; // Player space vertices (part 2) (x, y, z)
-			int32_t sv[4][2]; // Screen space vertices (x, y)
+			vec3s_t wv[4]; // World space vertices
+			vec3s_t pv1[4]; // Player space vertices (part 1)
+			vec3s_t pv2[4]; // Player space vertices (part 2)
+			vec2i_t sv[4]; // Screen space vertices
 
 			// Current cos and sin of player yaw
 			scalar_t cs = math.cos[player.angle.y];
 			scalar_t sn = math.sin[player.angle.y];
 
 			// World space vertices
-			wv[0][0] = SCALAR(40);
-			wv[0][1] = SCALAR(10);
-			wv[0][2] = SCALAR(0);
+			wv[0].x = SCALAR(40);
+			wv[0].y = SCALAR(10);
+			wv[0].z = SCALAR(0);
 
-			wv[1][0] = SCALAR(40);
-			wv[1][1] = SCALAR(290);
-			wv[1][2] = SCALAR(0);
+			wv[1].x = SCALAR(40);
+			wv[1].y = SCALAR(290);
+			wv[1].z = SCALAR(0);
 
-			wv[2][0] = SCALAR(40);
-			wv[2][1] = SCALAR(10);
-			wv[2][2] = SCALAR(64);
+			wv[2].x = SCALAR(40);
+			wv[2].y = SCALAR(10);
+			wv[2].z = SCALAR(64);
 
-			wv[3][0] = SCALAR(40);
-			wv[3][1] = SCALAR(290);
-			wv[3][2] = SCALAR(64);
+			wv[3].x = SCALAR(40);
+			wv[3].y = SCALAR(290);
+			wv[3].z = SCALAR(64);
 
 			// Player space vertices (part 1)
-			pv1[0][0] = wv[0][0] - player.origin.x;
-			pv1[0][1] = wv[0][1] - player.origin.y;
-			pv1[0][2] = wv[0][2] - player.origin.z;
+			pv1[0].x = wv[0].x - player.origin.x;
+			pv1[0].y = wv[0].y - player.origin.y;
+			pv1[0].z = wv[0].z - player.origin.z;
 
-			pv1[1][0] = wv[1][0] - player.origin.x;
-			pv1[1][1] = wv[1][1] - player.origin.y;
-			pv1[1][2] = wv[1][2] - player.origin.z;
+			pv1[1].x = wv[1].x - player.origin.x;
+			pv1[1].y = wv[1].y - player.origin.y;
+			pv1[1].z = wv[1].z - player.origin.z;
 
-			pv1[2][0] = wv[2][0] - player.origin.x;
-			pv1[2][1] = wv[2][1] - player.origin.y;
-			pv1[2][2] = wv[2][2] - player.origin.z;
+			pv1[2].x = wv[2].x - player.origin.x;
+			pv1[2].y = wv[2].y - player.origin.y;
+			pv1[2].z = wv[2].z - player.origin.z;
 
-			pv1[3][0] = wv[3][0] - player.origin.x;
-			pv1[3][1] = wv[3][1] - player.origin.y;
-			pv1[3][2] = wv[3][2] - player.origin.z;
+			pv1[3].x = wv[3].x - player.origin.x;
+			pv1[3].y = wv[3].y - player.origin.y;
+			pv1[3].z = wv[3].z - player.origin.z;
 
 			// Player space vertices (part 2)
-			pv2[0][0] = MUL(pv1[0][0], cs) - MUL(pv1[0][1], sn);
-			pv2[0][1] = MUL(pv1[0][1], cs) + MUL(pv1[0][0], sn);
-			pv2[0][2] = 0 - pv1[0][2] + DIV(MUL(SCALAR(player.angle.x), pv2[0][1]), SCALAR(32));
+			pv2[0].x = MUL(pv1[0].x, cs) - MUL(pv1[0].y, sn);
+			pv2[0].y = MUL(pv1[0].y, cs) + MUL(pv1[0].x, sn);
+			pv2[0].z = 0 - pv1[0].z + DIV(MUL(SCALAR(player.angle.x), pv2[0].y), SCALAR(32));
 
-			pv2[1][0] = MUL(pv1[1][0], cs) - MUL(pv1[1][1], sn);
-			pv2[1][1] = MUL(pv1[1][1], cs) + MUL(pv1[1][0], sn);
-			pv2[1][2] = 0 - pv1[1][2] + DIV(MUL(SCALAR(player.angle.x), pv2[1][1]), SCALAR(32));
+			pv2[1].x = MUL(pv1[1].x, cs) - MUL(pv1[1].y, sn);
+			pv2[1].y = MUL(pv1[1].y, cs) + MUL(pv1[1].x, sn);
+			pv2[1].z = 0 - pv1[1].z + DIV(MUL(SCALAR(player.angle.x), pv2[1].y), SCALAR(32));
 
-			pv2[2][0] = MUL(pv1[2][0], cs) - MUL(pv1[2][1], sn);
-			pv2[2][1] = MUL(pv1[2][1], cs) + MUL(pv1[2][0], sn);
-			pv2[2][2] = 0 - pv1[2][2] + DIV(MUL(SCALAR(player.angle.x), pv2[2][1]), SCALAR(32));
+			pv2[2].x = MUL(pv1[2].x, cs) - MUL(pv1[2].y, sn);
+			pv2[2].y = MUL(pv1[2].y, cs) + MUL(pv1[2].x, sn);
+			pv2[2].z = 0 - pv1[2].z + DIV(MUL(SCALAR(player.angle.x), pv2[2].y), SCALAR(32));
 
-			pv2[3][0] = MUL(pv1[3][0], cs) - MUL(pv1[3][1], sn);
-			pv2[3][1] = MUL(pv1[3][1], cs) + MUL(pv1[3][0], sn);
-			pv2[3][2] = 0 - pv1[3][2] + DIV(MUL(SCALAR(player.angle.x), pv2[3][1]), SCALAR(32));
+			pv2[3].x = MUL(pv1[3].x, cs) - MUL(pv1[3].y, sn);
+			pv2[3].y = MUL(pv1[3].y, cs) + MUL(pv1[3].x, sn);
+			pv2[3].z = 0 - pv1[3].z + DIV(MUL(SCALAR(player.angle.x), pv2[3].y), SCALAR(32));
 
 			// Screen space vertices (with divide-by-zero checking)
-			if (pv2[0][1] > SCALAR(0) && pv2[1][1] > SCALAR(0))
 			{
-				if (pv2[0][1] <= SCALAR(0))
+				if (pv2[0].y <= SCALAR(0))
 				{
-					ClipWall(&pv2[0][0], &pv2[0][1], &pv2[0][1], pv2[1][0], pv2[1][1], pv2[1][2]);
-					ClipWall(&pv2[2][0], &pv2[2][1], &pv2[2][1], pv2[3][0], pv2[3][1], pv2[3][2]);
+					ClipWall(&pv2[0].x, &pv2[0].y, &pv2[0].z, pv2[1].x, pv2[1].y, pv2[1].z);
+					ClipWall(&pv2[2].x, &pv2[2].y, &pv2[2].z, pv2[3].x, pv2[3].y, pv2[3].z);
 				}
 
-				if (pv2[1][1] <= SCALAR(0))
+				if (pv2[1].y <= SCALAR(0))
 				{
-					ClipWall(&pv2[1][0], &pv2[1][1], &pv2[1][1], pv2[0][0], pv2[0][1], pv2[0][2]);
-					ClipWall(&pv2[3][0], &pv2[3][1], &pv2[3][1], pv2[2][0], pv2[2][1], pv2[2][2]);
+					ClipWall(&pv2[1].x, &pv2[1].y, &pv2[1].z, pv2[0].x, pv2[0].y, pv2[0].z);
+					ClipWall(&pv2[3].x, &pv2[3].y, &pv2[3].z, pv2[2].x, pv2[2].y, pv2[2].z);
 				}
 
-				sv[0][0] = ScalarToInteger(DIV(MUL(pv2[0][0], SCALAR(200)), pv2[0][1])) + (SCREEN_WIDTH / 2);
-				sv[0][1] = ScalarToInteger(DIV(MUL(pv2[0][2], SCALAR(200)), pv2[0][1])) + (SCREEN_HEIGHT / 2);
+				sv[0].x = ScalarToInteger(DIV(MUL(pv2[0].x, SCALAR(200)), pv2[0].y)) + (SCREEN_WIDTH / 2);
+				sv[0].y = ScalarToInteger(DIV(MUL(pv2[0].z, SCALAR(200)), pv2[0].y)) + (SCREEN_HEIGHT / 2);
 
-				sv[1][0] = ScalarToInteger(DIV(MUL(pv2[1][0], SCALAR(200)), pv2[1][1])) + (SCREEN_WIDTH / 2);
-				sv[1][1] = ScalarToInteger(DIV(MUL(pv2[1][2], SCALAR(200)), pv2[1][1])) + (SCREEN_HEIGHT / 2);
+				sv[1].x = ScalarToInteger(DIV(MUL(pv2[1].x, SCALAR(200)), pv2[1].y)) + (SCREEN_WIDTH / 2);
+				sv[1].y = ScalarToInteger(DIV(MUL(pv2[1].z, SCALAR(200)), pv2[1].y)) + (SCREEN_HEIGHT / 2);
 
-				sv[2][0] = ScalarToInteger(DIV(MUL(pv2[2][0], SCALAR(200)), pv2[2][1])) + (SCREEN_WIDTH / 2);
-				sv[2][1] = ScalarToInteger(DIV(MUL(pv2[2][2], SCALAR(200)), pv2[2][1])) + (SCREEN_HEIGHT / 2);
+				sv[2].x = ScalarToInteger(DIV(MUL(pv2[2].x, SCALAR(200)), pv2[2].y)) + (SCREEN_WIDTH / 2);
+				sv[2].y = ScalarToInteger(DIV(MUL(pv2[2].z, SCALAR(200)), pv2[2].y)) + (SCREEN_HEIGHT / 2);
 
-				sv[3][0] = ScalarToInteger(DIV(MUL(pv2[3][0], SCALAR(200)), pv2[3][1])) + (SCREEN_WIDTH / 2);
-				sv[3][1] = ScalarToInteger(DIV(MUL(pv2[3][2], SCALAR(200)), pv2[3][1])) + (SCREEN_HEIGHT / 2);
+				sv[3].x = ScalarToInteger(DIV(MUL(pv2[3].x, SCALAR(200)), pv2[3].y)) + (SCREEN_WIDTH / 2);
+				sv[3].y = ScalarToInteger(DIV(MUL(pv2[3].z, SCALAR(200)), pv2[3].y)) + (SCREEN_HEIGHT / 2);
 
 				// RenderWall
-				RenderWall(&pic_bbuffer, sv[0][0], sv[1][0], sv[0][1], sv[1][1], sv[2][1], sv[3][1], 159);
+				RenderWall(&pic_bbuffer, sv[0].x, sv[1].x, sv[0].y, sv[1].y, sv[2].y, sv[3].y, 159);
 			}
 
 			// Print some console info
 			sprintf(console_buffer, "px: %d py: %d pz: %d", ScalarToInteger(player.origin.x), ScalarToInteger(player.origin.y), ScalarToInteger(player.origin.z));
 			Console::AddText(0, 0, console_buffer);
-			sprintf(console_buffer, "sv0: %d sv1: %d", sv[0][0], sv[0][1]);
+			sprintf(console_buffer, "sv0: %d sv1: %d", sv[0].x, sv[0].y);
 			Console::AddText(0, 1, console_buffer);
-			sprintf(console_buffer, "sv0: %d sv1: %d", sv[1][0], sv[1][1]);
+			sprintf(console_buffer, "sv0: %d sv1: %d", sv[1].x, sv[1].y);
 			Console::AddText(0, 2, console_buffer);
 		}
 
