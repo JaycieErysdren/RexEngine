1904f1a1458a6f6bb19fbaf0d92bf4fe0e3ea869
Jaycie Ewald <jaycie.ewald@outlook.com>
2022-12-20 10:21:49 -0600

    reorganize content folder to cleanup

--- content/mapsrc/yasha/test_kenney.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="1bit" tilewidth="17" tileheight="17" tilecount="961" columns="31" objectalignment="top">
- <tileoffset x="0" y="1"/>
- <grid orientation="orthogonal" width="1" height="1"/>
- <image source="tileset_legacy.png" width="543" height="543"/>
-</tileset>
---
+++ content/vivaduke/mapsrc/erysdren/walls.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="walls" tilewidth="128" tileheight="128" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../builddos/tex_bmp/duke3d/w000.bmp"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../builddos/tex_bmp/duke3d/w001.bmp"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../builddos/tex_bmp/duke3d/w002.bmp"/>
+ </tile>
+</tileset>
