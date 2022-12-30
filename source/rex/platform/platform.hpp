ea0c9d08ad32960e54c14e492875e3e4dbe33384
Jaycie Ewald <jaycie.ewald@outlook.com>
2022-12-30 06:59:06 -0600

    ugh im an idiot

---
+++ source/rex/platform/platform.hpp
@@ -0,0 +1,64 @@
+// ========================================================
+//
+// FILE:			/source/rex/platform/platform.hpp
+//
+// AUTHORS:			Jaycie Ewald
+//
+// PROJECT:			Rex Engine
+//
+// LICENSE:			ACSL v1.4
+//
+// DESCRIPTION:		Rex Engine: Platform header
+//
+// LAST EDITED:		December 30th, 2022
+//
+// ========================================================
+
+#ifndef __REX_PLATFORM_H__
+#define __REX_PLATFORM_H__
+
+//
+//
+// Rex Engine: Platform
+//
+//
+
+namespace Rex
+{
+
+//
+// Bootstrap
+//
+
+// Initialize platform-specific handlers
+bool Platform_Init();
+
+// Shutdown platform-specific handlers
+bool Platform_Shutdown();
+
+//
+// Devices
+//
+
+// Read data from all currently connected input devices (keyboards, mice, etc)
+bool Platform_ReadInputDevices();
+
+// Get mouse data
+bool Platform_MouseGet(rex_int *mx, rex_int *my, rex_int *mb);
+
+// Set mouse data
+bool Platform_MouseSet(rex_int mx, rex_int my, rex_int mb);
+
+// Returns true if the specified key is pressed
+bool Platform_KeyboardGet(rex_int scancode);
+
+//
+// Timer
+//
+
+// Returns the number of ticks elapsed since program start
+rex_int64 Platform_GetTicks64();
+
+} // namespace Rex
+
+#endif // __REX_PLATFORM_H__
